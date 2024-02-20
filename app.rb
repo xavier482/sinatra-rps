561abcc810d21a2209d00784b5922ef0f5813e85
xavier482 <143520650+xavier482@users.noreply.github.com>
2024-02-20 15:42:33 +0000

    finish gets

--- app.rb
+++ app.rb
@@ -7,3 +7,30 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/") do
+  erb(:rules)
+end
+
+get("/") do
+ erb(:rules)
+end
+
+get("/rock") do
+ move1 = ["rock", "paper", "scissors"]
+ @they1 = move1.sample
+erb(:rock)
+end
+
+get("/paper") do
+  move1 = ["rock", "paper", "scissors"]
+  @they1 = move1.sample
+  erb(:paper)
+ end
+ 
+ get("/scissors") do
+  move1 = ["rock", "paper", "scissors"]
+  @they1 = move1.sample
+  erb(:scissors)
+ end
+ 
